7a3f6c152d1d63edcc935040dda47d3f27ceb87d
Giovana <giamorim2101@gmail.com>
2024-08-13 10:50:56 -0300

    mais validação

--- script.js
+++ script.js
@@ -19,16 +19,21 @@ function validaCpf(cpf) {
 
     cpf = cpf.trim(); /*Retira os espaços*/
 
+    //Verificar se é composto SOMENTE de números, "." ou "-"
     if(/[a-zA-Z]/.test(cpf)) {  /*Para procurar letras maiusculas e miniusculas no CPF*/
         alert("CPF não pode conter letras.");
         return false;
     }
 
-    if(!/^[\d.-]+$/.test(cpf)) {
+    if(!/^[\d.-]+$/.test(cpf)) {  /*Serve para validar número, '.' e  o '-' atravês do [\d.-] e para não validar os outros caracteres atraves do ^ +$*/
         alert("O CPF só pode ter números, '.' ou '-'");
         return false;
     }
-    //Verificar se é composto SOMENTE de números, "." ou "-"
+    
+    if(cpf.length != 11 && cpf.length != 14) {
+        alert("Formato inválido!");
+        return false;
+    }
 
     return true;
 
