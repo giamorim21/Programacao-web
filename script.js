76a07797af4b739b6fa6a070613a00def79effa7
Giovana <giamorim2101@gmail.com>
2024-08-13 10:38:24 -0300

    mais validações

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function submeter() {
     let cpf = document.getElementById("cpf").value;
     let idade = document.getElementById("idade").value;
 
+    //console.log(cpf);
     console.log(validaCpf(cpf));
 
 }
@@ -12,11 +13,22 @@ function submeter() {
 function validaCpf(cpf) {
 
     if(cpf == "") {
-
         alert("Campo CPF não pode ser vazio.");
         return false;
-
     }
+
+    cpf = cpf.trim(); /*Retira os espaços*/
+
+    if(/[a-zA-Z]/.test(cpf)) {  /*Para procurar letras maiusculas e miniusculas no CPF*/
+        alert("CPF não pode conter letras.");
+        return false;
+    }
+
+    if(!/^[\d.-]+$/.test(cpf)) {
+        alert("O CPF só pode ter números, '.' ou '-'");
+        return false;
+    }
+    //Verificar se é composto SOMENTE de números, "." ou "-"
 
     return true;
 
